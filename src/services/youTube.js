6c85a8bf7a9efd2d5054c0a7c10ae42f38b3c294
smmakowski <makowski.sm@gmail.com>
2017-02-26 18:51:18 -0800

    Add function to service body

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,6 +1,27 @@
 angular.module('video-player')
-.service('youTube', function() {
-  this.search = function() {
-    console.log(this);
+
+.service('youTube', function($http, $window) {
+  this.search = function(query, callback) {
+    $http.get('http://www.googleapis.com/youtube/vs/search', {
+    	params: {
+    		part: 'snippet',
+    		q: query,
+    		type: 'video',
+    		maxResults: 5,
+    		key = $window.YOUTUBE_API_KEY,
+    		videoEmbeddable: 'true'
+    	}
+    })
+	
+	.then(function({data}) {
+		if (callback) {
+			callback(data.items);
+		}
+	})
+	.catch(function({data}) {
+		data.error.errors.forEach(function(err) {
+			console.log(err.message);
+		});
+	});
   };
 });
